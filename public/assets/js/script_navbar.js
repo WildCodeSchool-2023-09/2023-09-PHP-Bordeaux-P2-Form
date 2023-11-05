4e3fd5cf4a21e883193f4aa43fee905104d486ad
yoyodes1000 <yoyodes1000@gmail.com>
2023-11-05 12:36:14 +0100

    mini bug transition fixed in JS

--- public/assets/js/script_navbar.js
+++ public/assets/js/script_navbar.js
@@ -2,7 +2,15 @@ const hamburgerToggler = document.querySelector(".hamburger")
 const navlinksContainer = document.querySelector(".navlinks_container");
 
 const toggleNav = e => {
-    navlinksContainer.classList.toggle("open")
     hamburgerToggler.classList.toggle("open")
+    navlinksContainer.classList.toggle("open")
 }
 hamburgerToggler.addEventListener("click", toggleNav)
+
+new ResizeObserver(entries => {
+    if(entries[0].contentRect.width <= 600){
+        navlinksContainer.style.transition = "transform 0.3s ease-out"
+    } else {
+        navlinksContainer.style.transition = "none"
+    }
+}).observe(document.body)
