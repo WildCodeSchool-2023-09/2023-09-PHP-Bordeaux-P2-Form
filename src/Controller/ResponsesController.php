16e2c97656ffd9c42e40b90519b5e8ff895f70c3
Martha Smith <m.e.smith@live.co.uk>
2023-11-14 20:57:31 +0100

    tried to select more values

--- src/Controller/ResponsesController.php
+++ src/Controller/ResponsesController.php
@@ -7,11 +7,20 @@ use App\Controller\AbstractController;
 
 class ResponsesController extends AbstractController
 {
+  //  public function responses()
+  //  {
+   //     $responseSessionId = 3;
+  //      $responsesManager = new ResponsesManager();
+   //     $response = $responsesManager->getResponses($responseSessionId);
+
+       //    return $this->twig->render('Form/responses.html.twig', ['response' => $response]);
+ //   }
+
     public function responses()
     {
-        $responseSessionId = 1;
+        $userId = 3;
         $responsesManager = new ResponsesManager();
-        $response = $responsesManager->getResponses($responseSessionId);
+        $response = $responsesManager->getResponses($userId);
 
            return $this->twig->render('Form/responses.html.twig', ['response' => $response]);
     }
--- src/Model/ResponsesManager.php
+++ src/Model/ResponsesManager.php
@@ -8,19 +8,29 @@ class ResponsesManager extends AbstractManager
 {
     public const TABLE = 'completed_form';
 
-    public function getResponses($responseSessionId)
+  //  public function getResponses($responseSessionId) ************
+//   {
+  //      $query = "SELECT * FROM " . self::TABLE . " WHERE response_session_id = :response_session_id;";
+    //    $statement = $this->pdo->prepare($query);
+      //  $statement->bindValue(':response_session_id', $responseSessionId, \PDO::PARAM_STR);
+        //$statement->execute();
+        //$response = $statement->fetch();
+//
+    //    return $response;
+
+
+    public function getResponses($userId)
     {
-        $query = "SELECT * FROM " . self::TABLE . " WHERE response_session_id = :response_session_id;";
+        $query = "SELECT value, label, user_id
+        FROM completed_form
+        JOIN response_session ON response_session.id = completed_form.response_session_id
+        JOIN tool_form ON response_session.tool_form_id = tool_form.id
+        WHERE user_id = :user_id;";
         $statement = $this->pdo->prepare($query);
-        $statement->bindValue(':response_session_id', $responseSessionId, \PDO::PARAM_STR);
+        $statement->bindValue(':user_id', $userId, \PDO::PARAM_STR);
         $statement->execute();
         $response = $statement->fetch();
 
         return $response;
     }
 }
-  //  {
-   //     $statement = $this->connection->query('SELECT id, title FROM recipe');
-    //    $recipes = $statement->\PDO::FETCH_ASSOC
-
-   //     return $recipes;
