25da48949fe56ad1473c878ba7e443897c0de313
Martha Smith <m.e.smith@live.co.uk>
2023-11-15 11:21:38 +0100

    displayed individual responses

--- src/Controller/ResponsesController.php
+++ src/Controller/ResponsesController.php
@@ -7,21 +7,12 @@ use App\Controller\AbstractController;
 
 class ResponsesController extends AbstractController
 {
-  //  public function responses()
-  //  {
-   //     $responseSessionId = 3;
-  //      $responsesManager = new ResponsesManager();
-   //     $response = $responsesManager->getResponses($responseSessionId);
-
-       //    return $this->twig->render('Form/responses.html.twig', ['response' => $response]);
- //   }
-
     public function responses()
     {
-        $userId = 3;
+         $userId = 2;
         $responsesManager = new ResponsesManager();
-        $response = $responsesManager->getResponses($userId);
+        $responses = $responsesManager->getResponses($userId);
 
-           return $this->twig->render('Form/responses.html.twig', ['response' => $response]);
+        return $this->twig->render('Form/responses.html.twig', ['responses' => $responses]);
     }
 }
--- src/Model/ResponsesManager.php
+++ src/Model/ResponsesManager.php
@@ -8,20 +8,10 @@ class ResponsesManager extends AbstractManager
 {
     public const TABLE = 'completed_form';
 
-  //  public function getResponses($responseSessionId) ************
-//   {
-  //      $query = "SELECT * FROM " . self::TABLE . " WHERE response_session_id = :response_session_id;";
-    //    $statement = $this->pdo->prepare($query);
-      //  $statement->bindValue(':response_session_id', $responseSessionId, \PDO::PARAM_STR);
-        //$statement->execute();
-        //$response = $statement->fetch();
-//
-    //    return $response;
-
 
     public function getResponses($userId)
     {
-        $query = "SELECT value, label, user_id
+        $query = "SELECT label, value, user_id
         FROM completed_form
         JOIN response_session ON response_session.id = completed_form.response_session_id
         JOIN tool_form ON response_session.tool_form_id = tool_form.id
@@ -29,8 +19,8 @@ class ResponsesManager extends AbstractManager
         $statement = $this->pdo->prepare($query);
         $statement->bindValue(':user_id', $userId, \PDO::PARAM_STR);
         $statement->execute();
-        $response = $statement->fetch();
+        $responses = $statement->fetchAll();
 
-        return $response;
+        return $responses;
     }
 }
