cee337d8263a62f697e07928bb3ad103b19b6e74
Nicolas Leroux <hertford.fr@gmail.com>
2023-11-20 11:19:39 +0100

    abandon de array_map

--- src/Model/DataViewManager.php
+++ src/Model/DataViewManager.php
@@ -25,19 +25,12 @@ class DataViewManager
                 foreach ($choices as $choice) {
                     $data['questions'][$key]['choices'][$choice] = 0;
                 }
-                //$data[$key]['question']['choices'] = array_map(fn ($arr) => $arr['tool_option'],
-                //                                      $choiceManager->getChoices($question['id']));
             }
 
-            //var_dump($question['id']);
             $responses = $compFormManager->getResponsesForQuestion($question['id']);
-            //var_dump($responses);
-            //$data[$key]['question']['responses'] = $compFormManager->getResponsesForQuestion($question['id']);
             foreach ($responses as $response) {
                 $data['questions'][$key]['choices'][$response['value']] = $response['nb'];
             }
-            //$test = array_map(fn ($value) => [$value['value'] => $value['nb']], $responses);
-            //var_dump($test);
         }
 
         return $this->prepareCharts($data);
@@ -46,9 +39,6 @@ class DataViewManager
     public function prepareCharts($data)
     {
         foreach ($data['questions'] as $key => $question) {
-            /* if (!empty($question['question_type'] && $question['question_type'] == 'radio')) {
-                $data['questions'][$key]['dataPoints'] = $this->preparePieChart($question['choices']);
-            } */
             if ($question['question_type'] == 'radio' || $question['question_type'] == 'checkbox') {
                 $data['questions'][$key]['dataPoints'] = $this->preparePieChart($question['choices']);
             } elseif ($question['question_type'] == 'range') {
@@ -56,9 +46,6 @@ class DataViewManager
                 $data['questions'][$key]['average'] = $this->calcAverage($question['choices']);
             }
         }
-        // echo '<pre>';
-        // print_r($data);
-        // echo '<pre>';
         return $data;
     }
 
@@ -69,12 +56,6 @@ class DataViewManager
             $dataPoints['labels'][] = $key;
             $dataPoints['data'][] = $data;
         }
-        // $callback = function ($key, $value) {
-        //     $result['labels'][] = $key;
-        //     $result['values'][] = $value;
-        //     return $result;
-        // };
-        // $dataPoints = array_map($callback, array_keys($datas), $datas);
 
         return $dataPoints;
     }
@@ -91,11 +72,6 @@ class DataViewManager
         foreach ($question['choices'] as $key => $value) {
             $map[$key] = $value;
         }
-        // $callback = function ($key, $value) {
-        //     $result['labels'][] = $key;
-        //     $result['values'][] = $value;
-        // };
-        // $result = array_map($callback, array_keys($question['choices']), $question['choices']);
 
         foreach ($map as $x => $y) {
             $result['labels'][] = $x;
